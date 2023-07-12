b393d81f89fa4138b0bcd6e20ab88a8db06db796
unknown <juanpablo.cuadrelli@minimalart.co>
2023-07-12 10:55:59 -0300

    new convert

--- train/convert.js
+++ train/convert.js
@@ -4,17 +4,19 @@ const path = require('path');
 
 const FROM_FORMAT = "jpg"
 const TO_FORMAT = "jpeg"
-const FROM_DIR = `data${path.sep}temp`
+const FROM_DIR = `data${path.sep}temp3`
 const TO_DIR = `data${path.sep}train`
 const SIZE_W = 96
 const SIZE_H = 96
 // const LOW_QUALITY = 10
 // const HIGH_QUALITY = 90 
 const BLUR = 1
+const BRIGHTNESS = .4
 
 const convert = (dataDir) => {
   let filesToProccess = []
   var files = fs.readdirSync(dataDir);
+  console.log(files, 'FOTOS')
   for (let i = 0; i < files.length; i++) {
     if (!files[i].toLocaleLowerCase().endsWith("." + FROM_FORMAT)) {
       continue;
@@ -33,17 +35,19 @@ const convert = (dataDir) => {
       //and save it inside images folder using write() method.
       else {
         image.resize(SIZE_W,SIZE_H)
-
-        if (i.toLowerCase().includes("badquality")) {
-          // image.quality(LOW_QUALITY);
-          image.blur(BLUR)
-        }
+        // const blured = image.blur(BLUR)
+        // if (i.toLowerCase().includes("blur")) {
+        //   // image.quality(LOW_QUALITY);
+        //   image.blur(BLUR)
+        // }
 
         const newFileName = i
           .replace("." + FROM_FORMAT, "." + TO_FORMAT)
           .replace(FROM_DIR, TO_DIR)
           console.log("newFileName","./" + newFileName)
         image.write("./" + newFileName)
+        image.blur(BLUR).write("./" + newFileName.replace("goodQuality", "blured"))
+        image.brightness(BRIGHTNESS).write("./" + newFileName.replace("goodQuality", "bright"))
       }
     })
   }) 
